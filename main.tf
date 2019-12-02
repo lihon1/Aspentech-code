4a1e071daf060749d3a47c1fe8680758de46f40a
lihon1 <58066104+lihon1@users.noreply.github.com>
2019-12-02 17:24:41 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,3 @@ provider "aws" {
     secret_key   = "JAIVt9Uph7ZKC714r6edImzgWeyB4LQRSyIKW22x"
 }
 
-resource "tfe_organization" "test" {
-  name  = "Aspentech-www-test"
-  email = "alice.li@aspentech.com"
-}
-
-resource "tfe_workspace" "test" {
-  name         = "Aspentech-test"
-  organization = "Aspentech-www-test"
-}
