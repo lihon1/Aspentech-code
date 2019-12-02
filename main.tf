7952e2f6230f50f8a953327fc0155dce05cc881f
lihon1 <58066104+lihon1@users.noreply.github.com>
2019-12-02 13:52:44 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-resource "null_resource" "example" {
-- trigger = {
-- value = "example"
-- }
-+ trigger = {
-+ value = "example"
-+ }
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-2"
+}
+
+
+resource "aws_instance" "example" {
+  ami = "ami-0d5d9d301c853a04a"
+  instance_type = "t2.micro"
 }
