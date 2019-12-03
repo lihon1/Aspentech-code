88623e229f935dac1fb856e001c0e8e63e2cbcbf
lihon1 <58066104+lihon1@users.noreply.github.com>
2019-12-03 16:41:41 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,24 +5,12 @@ provider "aws" {
 }
 
 resource "tfe_organization" "test" {
-  name  = "Aspentech-www-prod"
+  name  = "Aspentech-www-t"
   email = "lhl_0625@163.com"
 }
 
 resource "tfe_workspace" "test" {
   name         = "Aspentech-code"
-  organization = "Aspentech-www-prod"
-}
-
-resource "tfe_organization_token" "test" {
-  organization = "Aspentech-www-prod"
-}
-
-resource "tfe_oauth_client" "test" {
-  organization     = "Aspentech-www-prod"
-  api_url          = "https://api.github.com"
-  http_url         = "https://github.com"
-  oauth_token      = "ot-f3Dw3NDLywkCS8wv"
-  service_provider = "github"
+  organization = "Aspentech-www-t"
 }
 
