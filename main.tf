e2ab2d3dccb8ede5b34795eb61adcfe591b211a8
lihon1 <58066104+lihon1@users.noreply.github.com>
2019-12-03 09:25:51 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,15 @@ provider "aws" {
     secret_key   = "JAIVt9Uph7ZKC714r6edImzgWeyB4LQRSyIKW22x"
 }
 
+resource "tfe_organization" "test" {
+  name  = "Aspentech-www-prod"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "Aspentech-code"
+  organization = "Aspentech-www-prod"
+}
+
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
