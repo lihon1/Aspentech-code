8dcf7c510be0016804da12ff008e74a097f88324
lihon1 <58066104+lihon1@users.noreply.github.com>
2019-12-03 09:21:40 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+    region           = "us-east-2"
+    access_key  = "AKIA5QMSYXFQMSJ4ZI23"
+    secret_key   = "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"
+}
+
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
