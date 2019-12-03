7bb77d94f9ef11c1109fdc40dda20a0244cdaed0
lihon1 <58066104+lihon1@users.noreply.github.com>
2019-12-03 09:32:33 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-provider "aws" {
-    region           = "us-east-2"
-    access_key  = "AKIA5QMSYXFQMSJ4ZI23"
-    secret_key   = "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"
-}
-
-resource "tfe_organization" "test" {
-  hostname = "app.terraform.io"
-  name  = "Aspentech-www-prod"
-  email = "lhl_0625@163.com"
-}
-
-resource "tfe_workspace" "test" {
-  hostname = "app.terraform.io"
-  name         = "Aspentech-code"
-  organization = "Aspentech-www-prod"
-}
-
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
