e6ef6849fb8613cf2b242667c7d372ab0a99b7ef
lihon1 <58066104+lihon1@users.noreply.github.com>
2019-12-03 09:47:14 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,27 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "Aspentech-www-prod"
-    workspaces {
-      name = "Aspentech-test"
-    }
-  }
+provider "aws" {
+    region           = "us-east-2"
+    access_key  = "AKIA5QMSYXFQMSJ4ZI23"
+    secret_key   = "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"
+}
+
+resource "tfe_organization" "test" {
+  name  = "Aspentech-www-prod"
+  email = "lhl_0625@163.com"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "Aspentech-code"
+  organization = "Aspentech-www-prod"
+}
+
+resource "tfe_organization_token" "test" {
+  organization = "Aspentech-www-prod"
+}
+
+resource "tfe_oauth_client" "test" {
+  organization     = "Aspentech-www-prod"
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  oauth_token      = "ot-f3Dw3NDLywkCS8wv"
+  service_provider = "github"
 }
