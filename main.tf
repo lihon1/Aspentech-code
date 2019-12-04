a36d9fe706ebeb60e1cd8c3b7cb41f7f33735c22
lihon1 <58066104+lihon1@users.noreply.github.com>
2019-12-04 10:36:25 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region           = "us-east-2"
+    access_key  = "AKIA5QMSYXFQPY6CECV6"
+    secret_key   = "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"
+}
+
+resource "tfe_organization" "test" {
+  name  = "Aspentech-www-prod"
+  email = "lhl_0625@163.com"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "Aspentech-code"
+  organization = "Aspentech-www-prod"
+}
