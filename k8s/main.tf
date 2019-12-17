6fd9144c234b1db24cb698acaf1b24c440018489
lihon1 <58066104+lihon1@users.noreply.github.com>
2019-12-17 10:15:00 +0800

    Update main.tf

--- k8s/main.tf
+++ k8s/main.tf
@@ -1,7 +1,17 @@
+# Configure the Azure provider
 provider "azurerm" {
-    version = "~>1.5"
+    subscription_id = "b0c1e8e7-c8f5-469e-ae08-b0840f3635a1"
+    client_id       = "f5ba78f2-d8e2-4048-aeef-5780042d8753"
+    client_secret   = "2f86d8a2-db7e-4aa6-bd78-9bb5691cb495"
+    tenant_id       = "fc489f72-fbbd-411f-af53-8b24d242161e"
 }
 
-terraform {
-    backend "azurerm" {}
+# Configuer a new resource group
+resource "azurerm_resource_group" "k8s" {
+    name     = "azure-k8stest"
+    location = "Central US"
+
+    tags = {
+        environment = "For Development"
+    }
 }
